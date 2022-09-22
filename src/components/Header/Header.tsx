3b43e8a44ae38a3e1eee44f0526405555751093d
Shibou <sadandcryalone@gmail.com>
2022-09-23 00:07:28 +0500

    init project, add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React, {FC} from 'react';
+import {HeaderWrapper, Logo, TextLogo, NavMenu, TextNav, SocialIcon, Container} from './header.style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+interface Props {
+    
+}
+ 
+const Header: FC<Props> = () => {
+    return ( 
+        <HeaderWrapper>
+            <Container>
+            <Logo>
+                <TextLogo>Music</TextLogo>
+                <TextLogo p={({
+                    fontWeight: '400',
+                })}>NFT Platform</TextLogo>
+            </Logo>
+            <NavMenu>
+                <TextNav>Marketplace</TextNav>
+                <TextNav>About</TextNav>
+                <TextNav>Resources</TextNav>
+                <TextNav>Faq</TextNav>
+            </NavMenu>
+            <SocialIcon>
+            <FontAwesomeIcon icon={faFacebookF} color="#fff" size="lg"></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faTwitter} color="#fff" size="lg"></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faInstagram} color="#fff" size="lg"></FontAwesomeIcon>
+            </SocialIcon>
+            </Container>
+        </HeaderWrapper>
+    );
+}
+ 
+export default Header;
---
+++ src/components/Header/header.style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+export interface Props {
+    p?: {
+    fontWeight: string,
+}
+}
+
+export const HeaderWrapper = styled.div`
+/* background-color: rgba(255, 255, 255, 0.1); */
+background-color: #000;
+display: flex;
+align-items: center;
+`
+export const Container = styled.div`
+padding: 28px 41px 28px 41px;
+display: flex;
+justify-content: space-between;
+width: 100%;
+align-items: center;
+
+`
+export const Logo = styled.div`
+display: flex;
+flex-direction: column;
+color: #fff;
+font-weight: 600;
+font-size: 20px;
+margin: 0;
+`
+export const TextLogo = styled.div<Props>`
+margin: 0;
+font-weight: ${props => props.p?.fontWeight};
+`
+export const NavMenu = styled.div`
+text-transform: uppercase;
+color: #fff;
+display: flex;
+`
+export const TextNav = styled.div`
+margin-right: 42px;
+font-size: 16px;
+`
+export const SocialIcon = styled.div`
+display: flex;
+`
